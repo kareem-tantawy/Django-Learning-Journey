566d6df0a04ee89f919d7510c85a3007ebaefdee
Karim Tantawy <kareemtantawy01@gmail.com>
2025-03-19 18:41:14 +0200

    added registration, login and logout

--- ChatterFlow/ChatterFlow/ChatterFlow/settings.py
+++ ChatterFlow/ChatterFlow/ChatterFlow/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +132,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
+LOGIN_REDIRECT_URL = 'blog-home'
--- ChatterFlow/ChatterFlow/ChatterFlow/urls.py
+++ ChatterFlow/ChatterFlow/ChatterFlow/urls.py
@@ -15,9 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login-form'),
+    # path('logout/', auth_views.LogoutView.as_view(
+    #     template_name='users/logout.html',
+    #     next_page='login-form',  # Optional: redirect after logout
+    #     http_method_names=['get', 'post']  # Allow GET method
+    # ), name='logout-form'),
+    path('logout/', user_views.logout_view, name='logout-form'),
+    path('register/', user_views.register, name='register-form'),
 ]
