4944a80371414163db5190671200bb9e0801d45d
Karim Tantawy <kareemtantawy01@gmail.com>
2025-03-20 23:47:21 +0200

    Added Profile page for the new users after registeration process, and added media content also

--- ChatterFlow/ChatterFlow/users/models.py
+++ ChatterFlow/ChatterFlow/users/models.py
@@ -5,9 +5,22 @@ from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    gender = models.CharField(max_length=10, default='Man')
+    image = models.ImageField(default='profile_pics/default_woman.jpg' if gender =='man' else 'profile_pics/default_man.jpg' , upload_to='profile_pics')
     bio = models.TextField(default='')
     
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+    def save(self, *args, **kwargs):
+        
+        super().save(*args, **kwargs)
+        
+        img = Image.open(self.image.path)
+        
+        width, height = img.size
+        min_size = min(width, height)
+        img = img.crop((0, 0, min_size, min_size))
+        
+        img.save(self.image.path)
     
